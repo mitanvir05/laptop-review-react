19d11ff0170d0d62e682897a78422f5ae4b35be5
mitanvir05 <tanvir.cse05@gmail.com>
2022-04-05 22:33:33 +0600

    done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>This is about</h1>
+            <h1 className='text-2xl font-semibold'>This website is about product reviews. User can reviews product with their valuable comment. User also can give ratings out of 5.</h1>
         </div>
     );
 };
